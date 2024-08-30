da6cfd52a09e0fcb5460e587c16526536534cd9f
Alexander Nohe <nohe@google.com>
2024-08-30 08:52:05 -0700

    James Phone call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,22 @@ function getHeaderHost() {
   return hostname;
 }
 
+function isHome(): bool {
+  if (getHeaderHost == "nohe-example") {
+    return true
+  }
+  return false
+}
+
 export default function Home() {
+  if (isHome()) {
+  return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       Welcome to my wonderful site. We are no longer accepting new signups!
+      </main>
+    )
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      Home Page of {getHeaderHost()}
